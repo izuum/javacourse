d88a8fe3e1eaaef476d4f2236545c86b7384884a
izuum <dstep.ds@gmail.com>
2021-12-08 21:47:25 +0300

    add task3 of day14(with tests)

---
+++ src/main/java/day14/Classes/Person.java
@@ -0,0 +1,28 @@
+package day14.Classes;
+
+public class Person {
+    private String name;
+    private int year;
+
+
+    public Person(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/main/java/day14/Task3.java
@@ -0,0 +1,39 @@
+package day14;
+
+import day14.Classes.Person;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        File file = new File("People.txt");
+
+        System.out.println(parseFileToObjList(file));
+    }
+    public static List<Person> parseFileToObjList(File file){
+        try {
+            Scanner scanner = new Scanner(file);
+            List<Person> people = new ArrayList<>();
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] arr = line.split(" ");
+                if (Integer.parseInt(arr[1])<0) {
+                    throw new IllegalArgumentException();
+                }else{
+                    Person person = new Person(arr[0], Integer.parseInt(arr[1]));
+                    people.add(person);
+                }
+            }
+            scanner.close();
+            return people;
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e){
+            System.out.println("Некорректный входной файл");
+        }
+        return null;
+    }
+}
