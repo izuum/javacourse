036e360deb8632398d313b76dde7c5daa98e07dd
izuum <dstep.ds@gmail.com>
2021-12-12 09:54:13 +0300

    modified class PathFile for use with enter any path

--- src/main/java/utility/PathFile.java
+++ src/main/java/utility/PathFile.java
@@ -1,10 +1,15 @@
 package utility;
+
 import java.io.File;
+import java.util.Scanner;
 
 public class PathFile {
     public static File pathDownloadsFile(String fileName) {
         String separator = File.separator;
-        File file = new File("src" + separator + "main" + separator + "resources" + separator + fileName).getAbsoluteFile();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter path: ");
+        String path = scanner.nextLine().replace("/", separator);
+        File file = new File(path + fileName).getAbsoluteFile();
         return file;
     }
 }
